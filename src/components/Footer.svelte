d5173e1b11aae753e11fe1483f3855b97a2b7cb3
Jéssica Campos <jessica.fraga01@hotmail.com>
2023-07-25 17:51:01 -0300

    add: initial html structure and routes

---
+++ src/components/Footer.svelte
@@ -0,0 +1,35 @@
+<footer class="footer">
+  <div class="container">
+    <p>
+      Este é um teste técnico realizado para o projeto Bloom. 2023, by
+      <a href="https://kuroi.com.br">Jéssica Fraga</a>.
+    </p>
+    <p>
+      <a
+        href="https://github.com/iKuroiDev/bloom-ecommerce"
+        target="_blank"
+        class="footer__icon"
+      >
+        <i class="fab fa-github" />
+      </a>
+    </p>
+  </div>
+</footer>
+
+<style>
+  .footer {
+    margin-top: auto;
+    background-color: #443f52;
+    color: #fff;
+    text-align: center;
+    padding: 30px 15px;
+  }
+
+  .footer__icon {
+    font-size: 1.5em;
+  }
+
+  a {
+    color: #fff;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Footer from "../components/Footer.svelte";
+  import Header from "../components/Header.svelte";
+  import "../styles/global.css";
+</script>
+
+<div class="content">
+  <Header />
+  <!-- page content -->
+  <slot />
+
+  <Footer />
+</div>
+
+<style>
+  .content {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
